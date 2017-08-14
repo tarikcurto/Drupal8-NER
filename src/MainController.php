413613d8bc2161c52a5dfc53532d7413cabca158
Tarik Curto <centro.tarik@live.com>
2017-08-14 19:17:29 +0200

    + Main Controller + Admin/Config links group

---
+++ src/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\ner;
+
+use \Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Default controller of module
+ *
+ * @package Drupal\ner
+ */
+class MainController extends ControllerBase{
+
+    /**
+     * NER home page.
+     *
+     * @return array
+     */
+    public function setup(){
+
+        return [
+
+        ];
+    }
+}
